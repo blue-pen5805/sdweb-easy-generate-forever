2f507a03be3dd918765de114dd35c4f703805548
blue.pen5805 <blue.pen5805@fastmail.com>
2023-05-12 20:34:20 +0900

    img2img でも表示されるようにした

--- javascript/easy_generate_forever.js
+++ javascript/easy_generate_forever.js
@@ -39,36 +39,38 @@ onUiLoaded(() => {
     indicator.innerHTML = !alert ? '♾️' : '🚫'
   }
 
-  const [generateForeverButton, cancelGenerateForeverButton] = buildButtons()
+  ['txt2img', 'img2img'].forEach((tab) => {
+    const [generateForeverButton, cancelGenerateForeverButton] = buildButtons()
 
-  const indicator = document.createElement('span')
-  indicator.classList.add('indicator')
-  setIndicatorLabel(indicator)
+    const indicator = document.createElement('span')
+    indicator.classList.add('indicator')
+    setIndicatorLabel(indicator)
 
-  const seedInput = gradioApp().getElementById('txt2img_seed').querySelector('input')
-  // FIXME: no dispatch event when `Send to txt2img`
-  setInterval(() => { setIndicatorLabel(indicator, seedInput.value !== '-1') }, 1000)
+    const seedInput = gradioApp().getElementById(`${tab}_seed`).querySelector('input')
+    // FIXME: no dispatch event when `Send to txt2img`
+    setInterval(() => { setIndicatorLabel(indicator, seedInput.value !== '-1') }, 1000)
 
-  //
-  generateForeverButton.addEventListener('click', () => {
-    generateOnRepeat('#txt2img_generate', '#txt2img_interrupt')
-    indicator.classList.add('forever')
+    //
+    generateForeverButton.addEventListener('click', () => {
+      generateOnRepeat(`#${tab}_generate`, `#${tab}_interrupt`)
+      indicator.classList.add('forever')
+    })
+    cancelGenerateForeverButton.addEventListener('click', () => {
+      cancelGenerateForever()
+      indicator.classList.remove('forever')
+    })
+
+    //
+    const buttons = document.createElement('div')
+    buttons.classList.add('easy_generate_forever_container', 'buttons')
+    buttons.appendChild(generateForeverButton)
+    buttons.appendChild(cancelGenerateForeverButton)
+
+    const container = document.createElement('div')
+    container.classList.add('easy_generate_forever_container')
+    container.appendChild(indicator)
+    container.appendChild(buttons)
+
+    gradioApp().getElementById(`${tab}_generate_box`).after(container)
   })
-  cancelGenerateForeverButton.addEventListener('click', () => {
-    cancelGenerateForever()
-    indicator.classList.remove('forever')
-  })
-
-  //
-  const buttons = document.createElement('div')
-  buttons.classList.add('easy_generate_forever_container', 'buttons')
-  buttons.appendChild(generateForeverButton)
-  buttons.appendChild(cancelGenerateForeverButton)
-
-  const container = document.createElement('div')
-  container.classList.add('easy_generate_forever_container')
-  container.appendChild(indicator)
-  container.appendChild(buttons)
-
-  gradioApp().getElementById('txt2img_generate_box').after(container)
 })
